bae3064e3a7ae5888142ff97b7cc7a3cf2dc6e5a
leobtt <leonarbat8@gmail.com>
2022-02-24 00:25:05 -0300

    hook useGetData

---
+++ src/hooks/useGetData.js
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../context'
+import { fire } from '../services'
+
+const useGetData = (path) => {
+  const { user } = useContext(UserContext)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      const ref = fire.database().ref(user.uid + path)
+      ref.on('value', (snapshot) => {
+        setData(snapshot.val())
+      })
+
+      return () => {
+        ref.off()
+      }
+    }
+  }, [])
+
+  return data
+}
+
+export default useGetData
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context'
 import { Navigate } from 'react-router-dom'
-import { fire } from '../services'
+import useGetData from '../hooks/useGetData'
 
 const Home = () => {
   const { user } = useContext(UserContext)
-  const [data, setData] = useState(null)
+  const data = useGetData('')
 
-  /* redirecionando */
-  if (!user) {
+  if (!user || user.length <= 1) {
     return <Navigate replace to="/login" />
   }
-
-  /* pegando dados */
-  useEffect(() => {
-    const ref = fire.database().ref(`${user.uid}/`)
-    ref.on('value', (snapshot) => {
-      setData(snapshot.val())
-    })
-  }, [])
 
   return (
     <div>
       <h1>home</h1>
       {user && <pre>{JSON.stringify(user, null, 3)}</pre>}
-      {<pre>{JSON.stringify(data, null, 3)}</pre>}
+      {data && <pre>{JSON.stringify(data, null, 3)}</pre>}
     </div>
   )
 }
