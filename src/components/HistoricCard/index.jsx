16f72bc78893e425a31cc7911d16e34518a800b1
leobtt <leonarbat8@gmail.com>
2022-03-01 00:06:25 -0300

    layout transaction card

--- src/components/HistoricCard/index.jsx
+++ src/components/HistoricCard/index.jsx
@@ -1,3 +1,4 @@
+import Dashboard from '@mui/icons-material/Dashboard'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useGetData from '../../hooks/useGetData'
@@ -6,7 +7,24 @@ import './historicCard.scss'
 const HistoricCard = () => {
   const { date } = useParams()
   const data = useGetData(`/movimentacoes/${date}`)
-  return <div>HistoricCard</div>
+  return (
+    <>
+      <h2 className="title">Transações</h2>
+      <div className="transaction">
+        <div className="transaction__value">
+          <span>R$1000,00</span>
+          <span>Convênio</span>
+        </div>
+        <div className="transaction__category">
+          <Dashboard style={{ fontSize: '40px' }} />
+          <span>Salário</span>
+        </div>
+        <div className="transaction__date">
+          <span>24/04/12</span>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default HistoricCard
--- src/pages/Home/Main/index.jsx
+++ src/pages/Home/Main/index.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './main.scss'
 import Header from './Header'
 import CardInfo from '../../../components/CardInfo'
+import HistoricCard from '../../../components/HistoricCard'
 
 const Main = () => {
   return (
     <div className="main">
       <Header />
-      <div className="content">
-        <div className="transactions">
+      <div className="main__content">
+        <div className="main__content__transactions">
           <CardInfo />
+          <HistoricCard />
         </div>
-        <div className="graphics"></div>
+        <div className="main__content__graphics">AQUI</div>
       </div>
     </div>
   )
